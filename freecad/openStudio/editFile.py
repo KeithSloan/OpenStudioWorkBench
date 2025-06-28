2975e8fcebf4967bcbed5220f76996c381c28a0b
Keith Sloan <keithsloan52@icloud.com>
2025-06-28 20:25:16 +0100

    gbXML2IDF edit commands

--- freecad/openStudio/editFile.py
+++ freecad/openStudio/editFile.py
@@ -24,20 +24,30 @@
 #                                                                          *
 ############################################################################
 
+def joinDir(path):
+    import os
+    
+    __dirname__ = os.path.dirname(__file__)
+    return os.path.join(__dirname__, path)
+
 def editFile(fname):
 	import FreeCAD
 	import subprocess,  os, sys
 
-	print(dir())
+	wrkDir = joinDir(os.path.join("..", "gbXML_IDF_wrk"))
+	fileName = os.path.join(wrkDir, fname)
+
+	#print(FreeCAD.getResourceDir())
+
 	editorPathName = FreeCAD.ParamGet(\
-		"User parameter:BaseApp/Preferences/Mod/OpenStudio").GetString('externalEditor')
+		"User parameter:BaseApp/Preferences/Mod/OpenStudio").GetString('ExternalEditor')
 	print(f"Path to external editor {editorPathName}")
     # ToDo : Check pathname valid                                      
 	if editorPathName != "":
 		# check file exists
 		# Check editor exists
 		p1 = subprocess.Popen( \
-            [editorPathName, fname], \
+            [editorPathName, fileName], \
             stdin=subprocess.PIPE,\
             stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	else:
